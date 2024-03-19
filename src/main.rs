57cc627b95cb33550d8feabcc87896c7481b8c3a
Zaki Machfj <zmachfj@uwaterloo.ca>
2024-03-19 09:53:25 -0400

    return OK

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                let mut buf = [0; 128];
+                stream.read(&mut buf)?;
+                let response = "HTTP/1.1 200 OK\r\n\r\n";
+                stream.write(response.as_bytes())?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+    Ok(())
 }
